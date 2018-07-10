c2edd05db0bc8f3868b95085af91607801bf473a
MiyasakaKazutoshi <miyasaka_kazutoshi@yahoo.co.jp>
2018-07-11 07:27:00 +0900

    add libraries for scraping & connecting

--- cfp_scraping.py
+++ cfp_scraping.py
@@ -10,14 +10,14 @@ import pymongo
 def main():
     cfps = []
     for i in range(1, 55):
-        print("===============================================")
-        print("page: {0}".format(i))
-        print("===============================================")
+        print(f"""===============================================
+               page: {i}
+===============================================""")
         rawData = fetchPageData(i)
         cfps = cfps + parseHTML(rawData)
 
     mongoClient = mongo().insert(cfps)
-    save_csv(cfps) #デバッグ用？
+    save_csv(cfps) # デバッグ用
 
 def fetchPageData(page_num):
     data = urlopen("https://fortee.jp/iosdc-japan-2018/proposal?page={0}".format(page_num))
@@ -37,7 +37,7 @@ def save_csv(cfps):
     with open('cfps_before.csv', 'w', newline = '', encoding = 'utf-8-sig') as f:
       writer = csv.DictWriter(f,CFP.csvHeader)
       writer.writeheader()
-      writer.writerows(map(lambda x: x.generate_document(),cfps))
+      writer.writerows(map(lambda x: x.generate_document(), cfps))
 
 
 class CFP:
@@ -45,49 +45,60 @@ class CFP:
         self.title = ""
         self.user = ""
         self.talk_type = ""
-        self.description = "" 
-        self.icon_url   = ""
-        self.twitter_id    = ""
+        self.description = ""
+        self.icon_url = ""
+        self.twitter_id = ""
 
-    csvHeader = ['title','user','talk_type','description','icon_url','twitter_id']
+    csvHeader = [
+            'title',
+            'user',
+            'talk_type',
+            'description',
+            'icon_url',
+            'twitter_id']
 
     def generate_document(self):
-        return {'title':self.title,'user':self.user,'talk_type':self.talk_type,'description':self.description,'icon_url':self.icon_url,'twitter_id':self.twitter_id} 
+        return {'title':self.title,
+                'user':self.user,
+                'talk_type':self.talk_type,
+                'description':self.description,
+                'icon_url':self.icon_url,
+                'twitter_id':self.twitter_id} 
 
     def desc(self):
-        print('-------------------------------------------------------------------')
-        print('title')
-        print(self.title)
-        print('\n')
+        print(f"""-------------------------------------------------------------------
+【title】
+{self.title}
 
-        print('user')
-        print(self.user)
-        print('\n')
+【user】
+{self.user}
 
-        print('talk_type')
-        print(self.talk_type)
-        print('\n')
+【talk_type】
+{self.talk_type}
 
-        print('description')
-        print(self.description)
-        print('\n')
+【description】
+{self.description}
+        
+【icon_url】
+{self.icon_url}
 
-        print('icon_url')
-        print(self.icon_url)
-        print('\n')
-
-        print('twitter_id')
-        print(self.twitter_id)
-        print('\n\n')
+【twitter_id】
+{self.twitter_id}
+                """)
 
     @classmethod
     def create(cls, cfpTree):
         cfp = CFP()
         cfp.title = cfpTree.xpath('./h2/a')[0].text
+        
         cfp.talk_type = cfpTree.xpath('./small')[0].text
-        before = cfpTree.xpath('.//div[contains(@class,"top20")]/span')[0].text_content()
-        cfp.user = re.sub(r'(\s|\t|)', "", before)
-        cfp.description = cfpTree.xpath('./div[contains(@class,"top40")]')[0].text_content() 
+
+        user_tmp = cfpTree.xpath('.//div[contains(@class,"top20")]/span')[0].text_content()
+        cfp.user = re.sub(r'^(\s|\t|　)+', "", user_tmp)
+
+        description_temp = cfpTree.xpath('./div[contains(@class,"top40")]')[0].text_content() 
+        cfp.description = re.sub(r'^(\s|\t|　)+', "", description_temp)
+
         icon_url_tree = cfpTree.xpath('.//span/img[contains(@class,"inline-avatar")]')
         if len(icon_url_tree) > 0:
             cfp.icon_url = 'https://fortee.jp' + cfpTree.xpath('.//span/img[contains(@class,"inline-avatar")]')[0].get('src')
@@ -103,7 +114,7 @@ class mongo:
     def __init__(self):
         # mongodb へのアクセスを確立
         self.client = pymongo.MongoClient('localhost', 27017)
-        self.database = self.client.iosdc2018
+        self.database = self.client.iosdc2018_phase_0
         self.collection = self.database.cfps
 
     def insert(self, cfps):
