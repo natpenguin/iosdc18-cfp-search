4b3c24a2b29003fc58cbec75e823a3ebc94fcde0
Yuji Taniguchi <natpenguin@macbook.local>
2018-07-13 22:24:43 +0900

    add header for JS

--- web/api.py
+++ web/api.py
@@ -7,12 +7,14 @@ class Resource(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
+        
 
         # TODO: 暫定修正
         client = pymongo.MongoClient(host='mongo', port=27017)
         db = client.iosdc2018_phase_0
         datas = db.cfps.find()
         resp.body = dumps(datas)
+        resp.append_header('Access-Control-Allow-Origin', '*')
 
         # mongo_response = cpm.cfp_mongo().find_all()
         # # 要変換処理
