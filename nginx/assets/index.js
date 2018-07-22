348abb273b7504a623c4286eec504fff62970029
Yusuke Hosonuma <tobi462@gmail.com>
2018-07-23 02:18:07 +0900

    add: adopted filter

--- nginx/assets/index.js
+++ nginx/assets/index.js
@@ -2,23 +2,41 @@ let proposalsMaster = [];
 
 // 検索フィールド
 const searchField = new Vue({
-    el: '#searchField',
+    el: '#searchCondition',
+    data: {
+        searchWord: '',
+        isAdopt: false
+    },
+    watch: {
+        searchWord: 'filter',
+        isAdopt: 'filter'
+    },
     methods: {
         filter: function(event) {
-            let text = event.target.value;
-            if (text.length > 0) {
-                let regText = new RegExp(text.trim(), 'i')
-                let filteredData = proposalsMaster.filter(
-                    value => 
-                    regText.test(value.title) ||
-                    regText.test(value.user) ||
-                    regText.test(value.twitter_id) ||
-                    regText.test(value.talk_type)
-                );
-                proposalsInstance.proposals = filteredData;
-            } else {
-                proposalsInstance.proposals = proposalsMaster;
-            }
+            let text = this.searchWord;
+            let isAdoptValue = this.isAdopt;
+            let isKeywordMatch = function(value) {
+                if (text.length > 0) {
+                    let regText = new RegExp(text.trim(), 'i')
+                    return regText.test(value.title) ||
+                        regText.test(value.user) ||
+                        regText.test(value.twitter_id) ||
+                        regText.test(value.talk_type);
+                } else {
+                    return true;
+                }
+            };
+            let isAdopted = function(value) {
+                if (isAdoptValue) {
+                    return value.is_adopted === true;
+                } else {
+                    return true;
+                }
+            };
+            let filteredData = proposalsMaster.filter(value =>
+                isKeywordMatch(value) && isAdopted(value)
+            );
+            proposalsInstance.proposals = filteredData;
         }
     }
 });
