4604498624679f22a86de14a37b372ec24983947
Yusuke Hosonuma <tobi462@gmail.com>
2018-07-31 03:44:24 +0900

    fix: sort asc by title

--- web/api.py
+++ web/api.py
@@ -10,7 +10,7 @@ class Resource(object):
         # TODO: 暫定修正
         client = pymongo.MongoClient(host='mongo', port=27017)
         db = client.iosdc2018_phase_2
-        datas = list(db.cfps.find({}, { '_id': False }))
+        datas = list(db.cfps.find({}, { '_id': False }).sort([("title", pymongo.ASCENDING)]))
         for data in datas:
             data['title'] = data['title'].strip()
             data['user'] = data['user'].strip()
