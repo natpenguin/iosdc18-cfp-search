d1dd728a92df2d6457c8421a12ab81f08e0320af
Yusuke Hosonuma <tobi462@gmail.com>
2018-08-26 15:29:38 +0900

    add: patch data orecon

--- scraper_patch_rejectcon/update_mongo.js
+++ scraper_patch_rejectcon/update_mongo.js
@@ -1,3 +1,7 @@
+//
+// Rejectcon:
+// https://iosdc-reject-conference.connpass.com/event/93314/
+//
 db.cfps.update({title: 'デザイナーと一緒にコラボして仕上げるアニメーション実装とショーケース紹介'}, {$set:{is_adopted_rejectcon:true}});
 db.cfps.update({title: 'DDD(ドメイン駆動設計)を知っていますか？？'}, {$set:{is_adopted_rejectcon:true}});
 db.cfps.update({title: 'UITesting with GraphQL'}, {$set:{is_adopted_rejectcon:true}});
@@ -14,3 +18,30 @@ db.cfps.update({title: 'Self-Documenting Code のススメ方'}, {$set:{is_adopt
 db.cfps.update({title: 'Flutter触りまくったからその知見を公開する'}, {$set:{is_adopted_rejectcon:true}});
 db.cfps.update({title: 'iOSアプリのUniversal対応をやめようとした話'}, {$set:{is_adopted_rejectcon:true}});
 db.cfps.update({title: '「開発初心者は何がわからないのか」わからないを言語化するヒント'}, {$set:{is_adopted_rejectcon:true}});
+
+// 
+// 俺コン:
+// https://orecon.connpass.com/event/94858/
+//
+db.cfps.update({title: '非同期UI描画による高速なアプリケーションの実装'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'ソースコードから読み解く - Quick はどのように実装されているのか？'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'Reduxで10本弱のアプリを作った弊社のナレッジ、時間の許す限り話すよ'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'アトミックデザインの導入と実践プロセス'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'UIWindowが複数存在するとき、ステータスバーのスタイル設定が難しい件'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'Adaptive UI - 解像度の異なるデバイスや画面の向きに対応する最適なレイアウトへ -'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'エンジニア経験を活かしたスクラムマスターとして開発チームとプロダクトを成長させる'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'Androidを考えたiOSアンチパターン'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'iOSでmp4をデコードして自分だけの動画プレーヤーを作ろう！'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'iPhoneでコスプレをする技術'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: '業務と両立できる！開発環境の改善！'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'もっとFluidでRedirectableなモーダル表示アニメーション'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: '勇気を出して、Appleにバグレポートを出してみませんか？'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'Swiftで聞いておぼえるテスト書き'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'シンボリケート／クラッシュログを人間が読めるようになるまで'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: '実例から学ぶモバイルアプリにおけるモーダル/モードレスの考え方'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'Textureは部分的に導入はできますか？'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'ログの発火テストをXCUITestで自動化しようとしたがUnitテストで実装した話'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'ブラックボックスなコードに対して少しずつテストを書いていくためのテクニック'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'iOS らしいデザインを実現するために HIG を改めて深掘りしよう！'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'iOSの理想的なUIを考える'}, {$set:{is_adopted_orecon:true}});
+db.cfps.update({title: 'SwiftNIO のアーキテクチャーとRxHttpClient'}, {$set:{is_adopted_orecon:true}});
