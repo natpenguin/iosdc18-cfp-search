1c4cc1837fe653e9101d0a9cf19068d747fe27e8
Yusuke Hosonuma <tobi462@gmail.com>
2018-12-08 02:50:47 +0900

    add: deploy script for firebase

---
+++ bin/deploy-firebase.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -xe
+
+cd `dirname $0`
+
+#
+# clean
+#
+if [ -d '../public' ]; then
+    rm -r ../public
+fi
+mkdir ../public
+
+#
+# save latest api response on local k8s
+#
+./deploy-local-k8s.sh
+curl -o ../public/api.json http://localhost/api
+./destroy-local-k8s.sh
+
+#
+# copy resources
+#
+cp ../nginx/index.html ../public
+cp -r ../nginx/assets ../public
+
+#
+# change endpoint
+#
+sed -i '' "s|axios.get('/api')|axios.get('/api.json')|" ../public/assets/index.js
+
+#
+# deploy
+#
+firebase deploy
